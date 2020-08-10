81ed431e0aa397e01fa615b56443e91d74abdb26
Dkrosch <vincent.meidianto1@gmail.com>
2020-08-10 21:38:42 +0700

    kuis 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,14 +12,18 @@
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
-Route::post('/items', 'ItemController@store'); // menyimpan data
-Route::get('/items', 'ItemController@index'); // menampilkan semua
-Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
-Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
-Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
-Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+Route::get('/proyek', 'ProjectsController@index'); //menampilkan tabel berisi data proyek-proyek
+Route::get('/proyek/create', 'ItemController@create'); // menampilkan form untuk membuat data proyek baru, di dalam form tersebut terdapat input pengisian data-data proyek dan penunjukkan manager
+Route::post('/proyek', 'ItemController@store'); // menyimpan proyek baru
+Route::get('/proyek/{id}/daftarkan-staff', 'ItemController@index'); // menampilkan halaman form untuk mendaftarkan staff baru dari data karyawan
+Route::post('/proyek/{id}/daftarkan-staff', 'ItemController@index'); // menyimpan data proyek yang sudah ditambahkan dengan staff-staff baru di halaman form daftarkan-staff
+Route::get('/proyek/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit data-data proyek
+Route::put('/proyek/{id}', 'ItemController@update'); // menyimpan data proyek yang sudah diedit melalui form edit proyek
+Route::delete('/proyek/{id}', 'ItemController@destroy'); // menghapus data dengan id tertentu
+Route::get('/karyawan', 'EmployeessController@index'); //menampilkan tabel berisi data karyawan
+Route::get('/karyawan/{id_karyawan}/daftar-proyek', 'EmployeesController@show'); //menampilkan data list proyek dimana saja karyawan tersebut terdaftar sebagai staff atau manager
